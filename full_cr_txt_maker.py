061053673fb8dd1a22fb001a3b5ac6b4e90d4eee
Magda Zalewska <m.jarzebska.zalewska@gmail.com>
2024-11-02 01:15:35 +0100

    Refactored code into functions

--- full_cr_txt_maker.py
+++ full_cr_txt_maker.py
@@ -9,15 +9,24 @@ def find_dir(dir_name, search_path="."):
     return results
 
 
-home_dir = Path.home()
-dir_path = find_dir("Client-Server-App", home_dir)
-files = os.listdir(dir_path[0])
-py_files_list = [file for file in files if file.endswith(".py")]
+def consolidate_files(folder_name, target_file):
+    # get path to the selected folder
+    home_dir = str(Path.home())
+    dir_path = find_dir(folder_name, home_dir)
 
-with open("full.cr.txt", "a+") as output_file:
-    for py_file in py_files_list:
-        with open(f"{dir_path[0]}/{py_file}", "r") as input_file:
-            contents = input_file.read()
-            output_file.write("\n" + "=" * 20 + "\n")
-            output_file.write(f"FILE:{py_file}" + "\n\n")
-            output_file.write(contents)
+    # list all py files in the source directory
+    files = os.listdir(dir_path[0])
+    py_files_list = [file for file in files if file.endswith(".py")]
+
+    # copy the contents of all py files into a single txt file
+    with open(target_file, "a+") as output_file:
+        output_file.truncate(0)
+        for py_file in py_files_list:
+            with open(f"{dir_path[0]}/{py_file}", "r") as input_file:
+                contents = input_file.read()
+                output_file.write("\n" + "=" * 20 + "\n")
+                output_file.write(f"FILE:{py_file}" + "\n\n")
+                output_file.write(contents)
+
+
+consolidate_files("Client-Server-App", "full.cr.txt")
