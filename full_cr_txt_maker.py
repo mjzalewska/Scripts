fa0ab1ba7fa6adbc334d2e8ae1992fe54bbef7cf
Magda Zalewska <m.jarzebska.zalewska@gmail.com>
2024-12-05 23:27:21 +0100

    Updated script due to path change

--- full_cr_txt_maker.py
+++ full_cr_txt_maker.py
@@ -15,14 +15,14 @@ def consolidate_files(folder_name, target_file):
     dir_path = find_dir(folder_name, home_dir)
 
     # list all py files in the source directory
-    files = os.listdir(dir_path[0])
+    files = os.listdir(dir_path[1])
     py_files_list = [file for file in files if file.endswith(".py")]
 
     # copy the contents of all py files into a single txt file
     with open(target_file, "a+") as output_file:
         output_file.truncate(0)
         for py_file in py_files_list:
-            with open(f"{dir_path[0]}/{py_file}", "r") as input_file:
+            with open(f"{dir_path[1]}/{py_file}", "r") as input_file:
                 contents = input_file.read()
                 output_file.write("\n" + "=" * 20 + "\n")
                 output_file.write(f"FILE:{py_file}" + "\n\n")
